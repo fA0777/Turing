c7c590168b4d3c90a6ddbefd85131166a871739a
Falling <3163450844@qq.com>
2023-12-05 23:18:13 +0800

    3

---
+++ Turing2/src/Turing2/src/Student.java
@@ -0,0 +1,91 @@
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private String studentId;
+    private char sex;
+    private int classNum;
+    private String mail;
+    //待办：信息建立时间、信息修改时间
+    private String setUpTime;
+    private String lastModifiedTime;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", studentId='" + studentId + '\'' +
+                ", sex=" + sex +
+                ", classNum=" + classNum +
+                ", mail='" + mail + '\'' +
+                ", setUpTime='" + setUpTime + '\'' +
+                ", lastModifiedTime='" + lastModifiedTime + '\'' +
+                '}';
+    }
+
+    public String getSetUpTime() {
+        return setUpTime;
+    }
+
+    public void setSetUpTime(String setUpTime) {
+        this.setUpTime = setUpTime;
+    }
+
+    public String getLastModifiedTime() {
+        return lastModifiedTime;
+    }
+
+    public void setLastModifiedTime(String lastModifiedTime) {
+        this.lastModifiedTime = lastModifiedTime;
+    }
+
+    public Student() {
+    }
+
+    public Student(String name, String studentId, String mail) {
+        this.name = name;
+        this.studentId = studentId;
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public int getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(int classNum) {
+        this.classNum = classNum;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+}
